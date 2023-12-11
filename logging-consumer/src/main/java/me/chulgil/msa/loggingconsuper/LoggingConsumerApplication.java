ecea6d1f6fce71bf0fb6717057297cae8d024502
chulgil <codenavi@gmail.com>
2023-12-11 17:04:05 +0900

    feat: add logging consumer

---
+++ logging-consumer/src/main/java/me/chulgil/msa/loggingconsuper/LoggingConsumerApplication.java
@@ -0,0 +1,11 @@
+package me.chulgil.msa.loggingconsuper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoggingConsumerApplication.class, args);
+    }
+}
