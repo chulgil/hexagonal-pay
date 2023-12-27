759860279af10ace50931e0f193aedb7146f20b1
chulgil <codenavi@gmail.com>
2023-12-27 16:19:55 +0900

    fix: edit for httpClient

---
+++ payment-service/src/main/java/me/chulgil/msa/payment/PaymentConfig.java
@@ -0,0 +1,9 @@
+package me.chulgil.msa.payment;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("me.chulgil.msa.common")
+public class PaymentConfig {
+}
